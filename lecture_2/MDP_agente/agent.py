c0fd53f9104f07dc3d5d6af8ef247b38acca9438
José Briceño <josejorgebriceno9@gmail.com>
2023-02-07 19:15:59 -0400

    Updated agent and main

--- lecture_2/MDP_agente/agent.py
+++ lecture_2/MDP_agente/agent.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pprint
 
 
 class MDPAgent():
@@ -16,20 +17,27 @@ class MDPAgent():
 
     def get_action(self, state):
         return int(self.policy[state])
-    
+
     def set_gamma(self, gamma):
         self.gamma = gamma
 
     def render(self):
-        print("Values: {}, Policy: {}".format(self.values, self.policy))
+        print("--------------------------------------------------------------------")
+        print(f'Data for gama {self.gamma}')
+        print("--------------------------------------------------------------------")
+        print(f"Values: {self.values}")
+        print("--------------------------------------------------------------------")
+        print(f"Policy: {self.policy}")
+        print("--------------------------------------------------------------------")
+        print("\n")
 
     def solve(self, iterations, mode='value-iteration'):
         if(mode == 'value-iteration'):
-            self.value_teration(iterations)
+            self.value_iteration(iterations)
         elif(mode == 'policy-iteration'):
             self.policy_iteration(iterations)
 
-    def value_teration(self, iterations):
+    def value_iteration(self, iterations):
         for t in range(iterations):
             for s in range(self.states_n):
                 values = [sum([prob * (r + self.gamma * self.values[s_next])
@@ -38,43 +46,36 @@ class MDPAgent():
                 self.values[s] = max(values)
                 self.policy[s] = np.argmax(np.array(values))
     
+    # Policy evaluation
+    def policy_evaluation(self, iterations):
+        for i in range(iterations):
+            for s in range(self.states_n):
+                values = [sum([prob * (r + self.gamma * self.values[s_next])
+                            for prob, s_next, r, _ in self.P[s][self.policy[s]]])]
+                self.values[s] = max(values)
+            self.policy_improvement()
+            if self.optimal_policy_found:
+                break
+
+
+    
+    def policy_improvement(self):
+        # Policy improvement
+        # With updated state values, improve policy if needed
+        self.optimal_policy_found = True
+
+        for s in range(self.states_n):
+            old_action = self.policy[s]
+            values = [sum([prob * (r + self.gamma * self.values[s_next])
+                            for prob, s_next, r, _ in self.P[s][a]])
+                        for a in range(self.actions_n)]
+            self.policy[s] = np.argmax(np.array(values))
+
+            if old_action != self.policy[s]:
+                self.optimal_policy_found = False
+
     def policy_iteration(self, iterations):
         # Policy evaluation
-        def policy_evaluation():
-            for i in range(iterations):
-                for s in range(self.states_n):
-                    values = [sum([prob * (r + self.gamma * self.values[s_next])
-                                for prob, s_next, r, _ in self.P[s][self.policy[s]]])]
-                    self.values[s] = max(values)
-                policy_improvement()
-                print(self.optimal_policy_found, i)
-                if self.optimal_policy_found:
-                    break
-
-        def policy_improvement():
-            # Policy improvement
-            # With updated state values, improve policy if needed
-            # optimal_policy_found = True # Initial assumption: policy is stable
-            self.optimal_policy_found = True
-
-            for s in range(self.states_n):
-                old_action = self.policy[s]
-                values = [sum([prob * (r + self.gamma * self.values[s_next])
-                                for prob, s_next, r, _ in self.P[s][a]])
-                            for a in range(self.actions_n)]
-                self.policy[s] = np.argmax(np.array(values))
-
-                if old_action != self.policy[s]:
-                    self.optimal_policy_found = False
-                
-            
-                # if optimal_policy_found:
-                #     break
-                # else: policy_evaluation()
-
-
-
-        # Compute value for each state under current policy
-        policy_evaluation()
+        self.policy_evaluation(iterations)
 
         
--- lecture_2/MDP_agente/main.py
+++ lecture_2/MDP_agente/main.py
@@ -10,35 +10,28 @@ if "SDL_AUDIODRIVER" in os.environ:
     del os.environ["SDL_AUDIODRIVER"]
 
 # RobotBattery-v0, FrozenLake-v1, FrozenLake-v2
-env = gym.make('FrozenLake-v1', render_mode="human", is_slippery=True)
+env = gym.make('FrozenLake-v2', render_mode="human", is_slippery=True)
 
-agent1 = MDPAgent(env.observation_space.n, env.action_space.n, env.P)
-agent2 = MDPAgent(env.observation_space.n, env.action_space.n, env.P)
+agent = MDPAgent(env.observation_space.n, env.action_space.n, env.P)
 
-gamma_array = [round(value*0.1, 2) for value in range(10)]
-
-for gamma in gamma_array:
-    agent1.set_gamma(gamma)
-    agent1.solve(10000, mode='value-iteration')
-    agent1.render()
-    agent1.reset()
+gamma_array = [round(value*0.1, 2) for value in range(1, 11)]
 
 for gamma in gamma_array:
-    agent2.set_gamma(gamma)
-    agent2.solve(10000, mode='policy-iteration')
-    agent2.render()
-    agent2.reset()
-    
+    agent.set_gamma(gamma)
+    agent.solve(10000, mode='value-iteration')
+    agent.render()
+    agent.reset()
 
-observation, info = env.reset()
-terminated, truncated = False, False
 
-env.render()
-time.sleep(2)
+# observation, info = env.reset()
+# terminated, truncated = False, False
 
-while not (terminated or truncated):
-    action = agent.get_action(observation)
-    observation, _, terminated, truncated, _ = env.step(action)
+# env.render()
+# time.sleep(2)
 
-time.sleep(2)
-env.close()
+# while not (terminated or truncated):
+#     action = agent.get_action(observation)
+#     observation, _, terminated, truncated, _ = env.step(action)
+
+# time.sleep(2)
+# env.close()
