3e667f67fd88829dca76cbad9df5c8a6965fc1a5
Jose Briceño <43896916+jjbriceno@users.noreply.github.com>
2023-02-07 14:25:23 -0400

    Updated agent logic for policy iteration

--- lecture_2/MDP_agente/agent.py
+++ lecture_2/MDP_agente/agent.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pprint
 
 
 class MDPAgent():
@@ -7,7 +8,7 @@ class MDPAgent():
         self.actions_n = actions_n
         self.P = P
         self.gamma = gamma
-        self.resul = True
+        self.optimal_policy_found = True
         self.reset()
 
     def reset(self):
@@ -18,7 +19,8 @@ class MDPAgent():
         return int(self.policy[state])
 
     def render(self):
-        print("Values: {}, Policy: {}".format(self.values, self.policy))
+        pprint.pprint(self.values)
+        pprint.pprint(self.policy)
 
     def solve(self, iterations, mode='value-iteration'):
         if(mode == 'value-iteration'):
@@ -27,14 +29,14 @@ class MDPAgent():
             self.policy_iteration(iterations)
 
     def value_teration(self, iterations):
-        for t in range(iterations):
+        for _ in range(iterations):
             for s in range(self.states_n):
-                values = [sum([prob * (r + self.gamma * self.values[s_next])
-                                for prob, s_next, r, _ in self.P[s][a]])
-                            for a in range(self.actions_n)]
+                values = [sum([prob * (r + self.gamma * self.values[s_])
+                               for prob, s_, r, _ in self.P[s][a]])
+                          for a in range(self.actions_n)]
                 self.values[s] = max(values)
                 self.policy[s] = np.argmax(np.array(values))
-    
+
     def policy_iteration(self, iterations):
         # Policy evaluation
         def policy_evaluation():
@@ -43,15 +45,14 @@ class MDPAgent():
                     values = [sum([prob * (r + self.gamma * self.values[s_next])
                                 for prob, s_next, r, _ in self.P[s][self.policy[s]]])]
                     self.values[s] = max(values)
+
                 policy_improvement()
-                print(self.optimal_policy_found, i)
                 if self.optimal_policy_found:
                     break
 
         def policy_improvement():
             # Policy improvement
             # With updated state values, improve policy if needed
-            # optimal_policy_found = True # Initial assumption: policy is stable
             self.optimal_policy_found = True
 
             for s in range(self.states_n):
@@ -63,15 +64,7 @@ class MDPAgent():
 
                 if old_action != self.policy[s]:
                     self.optimal_policy_found = False
-                
-            
-                # if optimal_policy_found:
-                #     break
-                # else: policy_evaluation()
-
 
 
         # Compute value for each state under current policy
         policy_evaluation()
-
-        
